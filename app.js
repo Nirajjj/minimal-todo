ab8b0b3dca5e42aaac1b8665d848928c19a21440
Niraj Parab <nirajparab007@gmail.com>
2024-01-20 18:31:47 +0530

    update code and remove unnecessary code

--- app.js
+++ app.js
@@ -25,106 +25,123 @@ plusIcon.addEventListener('click', ()=> {
 })
 // ----------------add text to box-----------------
 
+redTodoList = [];
+greenTodoList = [];
+aquaTodoList = [];
+purpleTodoList = [];
+
 let fourButton = document.querySelectorAll('.todo-add-button');
 
 let redTodoNum = 0;
+// redTodoNum = numone;
+// numone = 1;
 let greenTodoNum = 0;
 let aquaTodoNum = 0;
 let purpleTodoNum = 0;
+
+
+
 for (let btn of fourButton) {
   let color = btn.id;
   btn.addEventListener('click', () =>  {
-    if (color == 'red') {
-      redTodoNum++;
-    } else if (color == 'green') {
-      greenTodoNum++;
-    } else if (color == 'aqua') {
-      aquaTodoNum++;
-    } else if (color == 'purple') {
-      purpleTodoNum++;
-    }
-  let typeInput = document.querySelector("input[type='text']");
-  let dateInput = document.querySelector("input[type='date']");
-  let typeValue = typeInput.value;
-  let dateValue = dateInput.value;
-  let box = document.querySelector(`#${color}box`);
-    
+    let typeInput = document.querySelector("input[type='text']");
+    let dateInput = document.querySelector("#due-date");
+    let typeValue = typeInput.value;
+    let dateValue = dateInput.value;
+
     if (typeInput.value == '') {
       alert('look like you do not type any todo')
       
-    } else if ((color == 'red' && redTodoNum > 4) ||
+    }else if (color == 'red') {
+      redTodoNum++;
+      redTodoList.push({
+        name: typeValue,
+        dueDate: dateValue
+      })
+      typeInput.value = '';
+      dateInput.value = '';
+      addTodo(color, redTodoList);
+
+    } else if (color == 'green') {
+      greenTodoNum++;
+      greenTodoList.push({
+        name: typeValue,
+        dueDate: dateValue
+      })
+      typeInput.value = '';
+      dateInput.value = '';
+      addTodo(color, greenTodoList);
+
+    } else if (color == 'aqua') {
+      aquaTodoNum++;
+      aquaTodoList.push({
+        name: typeValue,
+        dueDate: dateValue
+      })
+      typeInput.value = '';
+      dateInput.value = '';
+      addTodo(color, aquaTodoList);
+
+    } else if (color == 'purple') {
+      purpleTodoNum++;
+      purpleTodoList.push({
+        name: typeValue,
+        dueDate: dateValue
+      })
+      typeInput.value = '';
+      dateInput.value = '';
+      addTodo(color, purpleTodoList);
+    }
+    })}
+ 
+    function addTodo(color, array) {
+      console.log(color);
+      let box = document.querySelector(`#${color}box`);
+     if ((color == 'red' && redTodoNum > 4) ||
     (color == 'green' && greenTodoNum > 5) ||
     (color == 'aqua' && aquaTodoNum > 6) ||
     (color == 'purple' && purpleTodoNum > 7)) {
       alert('Prioritizing a minimum number of essential tasks enhances focus and productivity, ensuring meaningful progress over overwhelming oneself with an extensive list. and that is the goal of minimal todo');
     } else  {
-      box.innerHTML += `<p>&#8226;</p> <p>${typeValue}</p><p>${dateValue}</p><div class="tick"><span class=><i class="fa-solid fa-check"></i></span></div>
-      <div class="xmark"><span><i class="fa-solid fa-xmark"></i></span></div>`;   
-      typeInput.value = '';
-      dateInput.value = '';
-  }
+      box.innerHTML='';
+      for (let i = 0; i < array.length; i++) {
+        console.log(i);
+        const todoObject = array[i];
+        console.log(array);
+        name = todoObject.name;
+        dueDate = todoObject.dueDate;
+        
+        let htmltext = `<p>&#8226;</p> 
+      <p>${name}</p>
+      <p>${dueDate}</p>
+      <div class="tick" onclick = "removeTodo(${color}TodoList, ${i}, '${color}');"><span><i class="fa-solid fa-check"></i></span></div>
+      <div class="xmark" onclick = "removeTodo(${color}TodoList, ${i}, '${color}');"><span><i class="fa-solid fa-xmark"></i></span></div>`; 
+      console.log(htmltext);
+      box.innerHTML += htmltext;
+      }
     }
-        
-  )
-}
+    }
 
-
-
-
-
-
-
-
-
-// fourButton.forEach((btn, i, array)=> {
-//   if (i == 0) {
-//     let redTodoNum = 1;
-//     btn.addEventListener('click', ()=>{
-//       let typeValue = typeInput.value;
-//       let dateValue = dateInput.value;
-//       let redbtn = document.querySelector('#red');
+  
+    function removeTodo(color, index, colorname) {
       
-//       if (typeInput.value == '') {
-//         alert('look like you do not type any todo')
-//       } else if (redTodoNum > 4) {
-//         alert('Prioritizing a minimum number of essential tasks enhances focus and productivity, ensuring meaningful progress over overwhelming oneself with an extensive list. and that is the goal of minimal todo')
-//       } else  {
-//         redbtn.innerHTML += `<p>&#8226;</p> <p>${typeValue}</p>
-//         <p>${dateValue}</p>
-//         <div class="tick"><span class=><i class="fa-solid fa-check"></i></span></div>
-//         <div class="xmark"><span><i class="fa-solid fa-xmark"></i></span></div>
-//         `;
-//         redTodoNum++;   
-//       }
-          
+      let  array = color;
+      array.splice(index, 1);
       
-//       typeInput.value = '';
-//   })} else if (i == 1) {
-
-//     let greenTodoNum = 1;
-//     btn.addEventListener('click', ()=>{
-
-//       let typeValue = typeInput.value;
-//       let dateValue = dateInput.value;
-//       let greenbtn = document.querySelector('#green');
-      
-//       if (typeInput.value == '') {
-//         alert('look like you do not type any todo')
-        
-//       } else if (greenTodoNum > 4) {
-//         alert('Prioritizing a minimum number of essential tasks enhances focus and productivity, ensuring meaningful progress over overwhelming oneself with an extensive list. and that is the goal of minimal todo')
-//       } else  {
-//         redbtn.innerHTML += `<p>&#8226;</p> <p>${typeValue}</p>
-//         <p>${dateValue}</p>
-//         <div class="tick"><span class=><i class="fa-solid fa-check"></i></span></div>
-//         <div class="xmark"><span><i class="fa-solid fa-xmark"></i></span></div>
-//         `;
-//         greenTodoNum++;   
-//       }
-          
-      
-//       typeInput.value = '';
-//   })}
+      // let todoNumber = `${colorname}TodoNum`;
+      // todoNumber--;
+      // `${colorname}'TodoNum'`--;
+      addTodo(colorname, array);
+      if (colorname == 'red') {
+        redTodoNum--;
+      } else if (colorname == 'green') {
+        greenTodoNum--;
+      } else if (colorname == 'aqua') {
+        aquaTodoNum--;
+      } else if (colorname == 'purple') {
+        purpleTodoNum--;
+      }
+    }
 
 
 
